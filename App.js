39a2f0f52dff3012343bc16d21f3db0c0d5819c4
Alessia Bellisario <github@bellisar.io>
2023-08-31 12:17:53 -0400

    first commit, configure polyfills

--- App.js
+++ App.js
@@ -1,10 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { polyfill as polyfillEncoding } from "react-native-polyfill-globals/src/encoding";
+import { polyfill as polyfillReadableStream } from "react-native-polyfill-globals/src/readable-stream";
+import { polyfill as polyfillFetch } from "react-native-polyfill-globals/src/fetch";
 
-export default function App() {
+import {
+  gql,
+  HttpLink,
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useSubscription,
+  useQuery,
+} from "@apollo/client";
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+polyfillReadableStream();
+polyfillEncoding();
+polyfillFetch();
+
+const MULTIPART_SUBSCRIPTION = gql`
+  subscription MySubscription {
+    aNewDieWasCreated {
+      die {
+        id
+        roll
+        sides
+        color
+      }
+    }
+  }
+`;
+
+// const QUERY = gql`
+//   query AllProductions {
+//     allProducts {
+//       id
+//       ... @defer {
+//         sku
+//       }
+//     }
+//   }
+// `;
+
+const link = new HttpLink({
+  uri: "http://localhost:4040/",
+  fetchOptions: {
+    reactNative: { textStreaming: true },
+  },
+});
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  const { loading, error, data } = useSubscription(MULTIPART_SUBSCRIPTION);
+  // const { loading, error, data } = useQuery(QUERY);
+  console.log({ loading, error, data: JSON.stringify(data, null, 2) });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {loading ? <Text>Loading...</Text> : ""}
+      {error ? <Text>Error</Text> : ""}
+      {data ? <Text>{data.aNewDieWasCreated.die.sides}</Text> : ""}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +73,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
