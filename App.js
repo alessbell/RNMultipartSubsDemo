63c19684bccd180775dd0ae7f594e1f2d1adb294
Alessia Bellisario <github@bellisar.io>
2023-09-01 10:28:55 -0400

    chore: log Networking import for reproduction

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
+import React from "react";
 import { polyfill as polyfillEncoding } from "react-native-polyfill-globals/src/encoding";
 import { polyfill as polyfillReadableStream } from "react-native-polyfill-globals/src/readable-stream";
 import { polyfill as polyfillFetch } from "react-native-polyfill-globals/src/fetch";
+
+import { Networking } from "react-native";
+import AndroidNetworking from "react-native/Libraries/Network/RCTNetworking.android.js";
+import IOSNetworking from "react-native/Libraries/Network/RCTNetworking.ios.js";
+
+// IOSNetworking is defined (module with e.g. sendRequest method and others),
+// Networking / AndroidNetworking is an empty object
+console.log({ Networking, AndroidNetworking, IOSNetworking });
 
 import {
   gql,
@@ -13,7 +22,7 @@ import {
 } from "@apollo/client";
 
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 polyfillReadableStream();
 polyfillEncoding();
@@ -32,16 +41,16 @@ const MULTIPART_SUBSCRIPTION = gql`
   }
 `;
 
-// const QUERY = gql`
-//   query AllProductions {
-//     allProducts {
-//       id
-//       ... @defer {
-//         sku
-//       }
-//     }
-//   }
-// `;
+const QUERY = gql`
+  query AllProductions {
+    allProducts {
+      id
+      ... @defer {
+        sku
+      }
+    }
+  }
+`;
 
 const link = new HttpLink({
   uri: "http://localhost:4040/",
@@ -56,19 +65,34 @@ const client = new ApolloClient({
 });
 
 function App() {
-  const { loading, error, data } = useSubscription(MULTIPART_SUBSCRIPTION);
-  // const { loading, error, data } = useQuery(QUERY);
-  console.log({ loading, error, data: JSON.stringify(data, null, 2) });
+  // const [showSubscription, setShowSubscription] = React.useState(true);
 
   return (
     <View style={styles.container}>
-      {loading ? <Text>Loading...</Text> : ""}
-      {error ? <Text>Error</Text> : ""}
-      {data ? <Text>{data.aNewDieWasCreated.die.sides}</Text> : ""}
       <StatusBar style="auto" />
+      <Button
+        title="Toggle subscription"
+        onPress={() => {
+          setShowSubscription(!showSubscription);
+        }}
+      />
+      {/* {showSubscription ? <SubscriptionComponent /> : ""} */}
     </View>
   );
 }
+
+// function SubscriptionComponent() {
+//   const { loading, error, data } = useSubscription(MULTIPART_SUBSCRIPTION);
+//   console.log({ loading, error, data: JSON.stringify(data, null, 2) });
+
+//   return (
+//     <>
+//       {loading ? <Text>Loading...</Text> : ""}
+//       {error ? <Text>Error</Text> : ""}
+//       {data ? <Text>{data.aNewDieWasCreated.die.sides}</Text> : ""}
+//     </>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
